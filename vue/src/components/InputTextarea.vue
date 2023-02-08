1cd0c951215dd2cd90d4c7a7bea0280d850c3b52
Artur M <artur-milkowski@tlen.pl>
2023-02-08 11:14:05 +0100

    Add the input textarea component

---
+++ vue/src/components/InputTextarea.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  id: { type: String },
+  placeholder: { type: String },
+  modelValue: { type: String },
+});
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <textarea
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :id="id"
+    class="border border-gray-900 hover:border-black focus:outline-none focus:border-black p-1 w-full"
+    :placeholder="placeholder"
+  ></textarea>
+</template>
